7077b1943cf388926678b54f07f412fe1db0a713
idoubi <me@idoubi.cc>
2026-03-20 04:25:00 -0700

    Revert "feat: URL-based routing with deep links"
    This reverts commit 4b339d222b6fd92c960863ce192e868e87898869.

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { StoreProvider } from "@/lib/store";
+import { AppSidebar } from "@/components/app-sidebar";
+import { ChatArea } from "@/components/chat-area";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+
+export default function Home() {
+  return (
+    <StoreProvider>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset className="h-svh overflow-hidden">
+          <ChatArea />
+        </SidebarInset>
+      </SidebarProvider>
+    </StoreProvider>
+  );
+}
